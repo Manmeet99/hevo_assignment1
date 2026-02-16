3609ccf48674b5fe5fd5ecb17547470969eeb3f4
Manmeet Singh <mshunjan99@gmail.com>
2026-02-16 19:41:33 +0530

    Add Postgres table schema

---
+++ sql/postgres_ddl.sql
@@ -0,0 +1,20 @@
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(255),
+    address JSON
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id),
+    status VARCHAR(50)
+);
+
+CREATE TABLE feedback (
+    id SERIAL PRIMARY KEY,
+    order_id INT UNIQUE REFERENCES orders(id),
+    feedback_comment TEXT,
+    rating INT
+);
